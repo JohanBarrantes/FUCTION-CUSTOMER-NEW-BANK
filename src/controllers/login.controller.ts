8e4c4a12ca35ab5224b58e7f64399a4ad937228d
Sebastian Barrantes <seb_0506@hotmail.com>
2025-11-17 14:46:10 -0500

    Fix bcripts type for deploy

--- src/controllers/login.controller.ts
+++ src/controllers/login.controller.ts
@@ -5,7 +5,7 @@ import customerService from "../services/customer.service";
 export const loginController = async (event: APIGatewayProxyEvent) => {
   try {
     const body = JSON.parse(event.body || "{}");
-
+    console.log(body)
     const result = await customerService.login(body);
 
     return successResponse(result);
